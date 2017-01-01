0fa2a3fbd735f2dc5eef64b4da969cdca5803017
Chris Fordham <chris@fordham-nagy.id.au>
2017-01-01 11:34:08 +1100

    Have test use api 1.5.

--- client/src/main/java/au/com/xhost/rightscale/api/client/RightScaleAPIClientTest.java
+++ client/src/main/java/au/com/xhost/rightscale/api/client/RightScaleAPIClientTest.java
@@ -10,7 +10,7 @@ public class RightScaleAPIClientTest
     public static void main( String[] args )
     {
         RightScaleAPIClient rightscale;
-        rightscale = new RightScaleAPIClient("rick@james.com", "charliemurphy", "1456", "1.0", "us-4.rightscale.com");
+        rightscale = new RightScaleAPIClient("rick@james.com", "charliemurphy", "1456", "1.5", "us-4.rightscale.com");
         rightscale.enableDebug();
         rightscale.login();
         System.out.print("Logged in "+rightscale.getUserName());
